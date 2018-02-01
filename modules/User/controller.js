0cb97fa26aaf7785eb9d061c4a98a11867b64fd8
Diogo Paza <diogopazacvel@gmail.com>
2018-02-01 15:32:54 -0200

    inicio trabalho

---
+++ modules/User/controller.js
@@ -0,0 +1,52 @@
+const Model = require( './model' )
+
+const ACTIONS_PATH = './../../'
+const create = require( ACTIONS_PATH + 'actions/create' )( Model )
+const find = require( ACTIONS_PATH + 'actions/find' )( Model )
+
+
+const findOne = ( req, res ) => {
+
+  const query = {
+    _id: req.params.id
+  }
+
+  return Model.findOne( query, ( err, data ) => {
+    if ( err ) return console.log( 'ERRO: ', err )
+
+    return res.json( data )
+  } )
+}
+
+const update = ( req, res ) => {
+  const query = {
+    _id: req.params.id
+  }
+  const mod = req.body
+  
+  Model.update( query, mod, ( err, data ) => {
+    if ( err ) return console.log( 'ERRO: ', err )
+
+    return res.json( data )
+  } )
+}
+
+const remove = ( req, res ) => {
+  const query = {
+    _id: req.params.id
+  }
+  
+  Model.remove( query, ( err, data ) => {
+    if ( err ) return console.log( 'ERRO: ', err )
+
+    return res.json( data )
+  } )
+}
+
+module.exports = {
+  create,
+  find,
+  findOne,
+  update,
+  remove 
+}
